ddad084a05156435faad4075f708fafb8ba4df50
Lucas <Lucas@lucas.com>
2024-01-28 21:48:12 +0100

    linter

--- hive/move.php
+++ hive/move.php
@@ -14,7 +14,7 @@ unset($_SESSION['error']);
 
 if (!isset($board[$from]))
     $_SESSION['error'] = 'Board position is empty';
-elseif ($board[$from][count($board[$from])-1][0] != $player)
+elseif ($board[$from][count($board[$from]) - 1][0] != $player)
     $_SESSION['error'] = "Tile is not owned by player";
 elseif ($hand['Q'])
     $_SESSION['error'] = "Queen bee is not played";
@@ -40,8 +40,10 @@ else {
         if ($all) {
             $_SESSION['error'] = "Move would split hive";
         } else {
-            if ($from == $to) $_SESSION['error'] = 'Tile must move';
-            elseif (isset($board[$to]) && $tile[1] != "B") $_SESSION['error'] = 'Tile not empty';
+            if ($from == $to)
+                $_SESSION['error'] = 'Tile must move';
+            elseif (isset($board[$to]) && $tile[1] != "B")
+                $_SESSION['error'] = 'Tile not empty';
             elseif ($tile[1] == "Q" || $tile[1] == "B") {
                 if (!slide($board, $from, $to))
                     $_SESSION['error'] = 'Tile must slide';
@@ -49,11 +51,15 @@ else {
         }
     }
     if (isset($_SESSION['error'])) {
-        if (isset($board[$from])) array_push($board[$from], $tile);
-        else $board[$from] = [$tile];
+        if (isset($board[$from]))
+            array_push($board[$from], $tile);
+        else
+            $board[$from] = [$tile];
     } else {
-        if (isset($board[$to])) array_push($board[$to], $tile);
-        else $board[$to] = [$tile];
+        if (isset($board[$to]))
+            array_push($board[$to], $tile);
+        else
+            $board[$to] = [$tile];
         $_SESSION['player'] = 1 - $_SESSION['player'];
         $db = include 'database.php';
         $stmt = $db->prepare('insert into moves (game_id, type, move_from, move_to, previous_id, state) values (?, "move", ?, ?, ?, ?)');
@@ -65,5 +71,3 @@ else {
 }
 
 header('Location: index.php');
-
-?>
--- hive/play.php
+++ hive/play.php
@@ -27,11 +27,9 @@ elseif (array_sum($hand) <= 8 && $hand['Q']) {
     $_SESSION['player'] = 1 - $_SESSION['player'];
     $db = include 'database.php';
     $stmt = $db->prepare('insert into moves (game_id, type, move_from, move_to, previous_id, state) values (?, "play", ?, ?, ?, ?)');
-    $stmt->bind_param('issis', $_SESSION['game_id'], $piece, $to, $_SESSION['last_move'], get_state());
+    $stmt->bind_param('issis', $_SESSION['game_id'], $piece, $to, $_SESSION['last_move'], getState());
     $stmt->execute();
     $_SESSION['last_move'] = $db->insert_id;
 }
 
 header('Location: index.php');
-
-?>
