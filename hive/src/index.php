c3b3596c6a84bffe9aa43707289412b90cab8033
Lucas <Lucas@lucas.com>
2024-01-29 18:45:07 +0100

    calculatePositions added

--- hive/src/index.php
+++ hive/src/index.php
@@ -8,201 +8,207 @@ use Lucas\Hive\Util;
 
 if (!isset($_SESSION['board'])) {
     header('Location: restart.php');
-    exit(0);
+    exit;
 }
+
 $board = $_SESSION['board'];
 $player = $_SESSION['player'];
 $hand = $_SESSION['hand'];
 
-$to = [];
-foreach (Util::$OFFSETS as $pq) {
-    foreach (array_keys($board) as $pos) {
-        $pq2 = explode(',', $pos);
-        $to[] = ($pq[0] + $pq2[0]) . ',' . ($pq[1] + $pq2[1]);
+$to = calculatePositions($board);
+
+
+function calculatePositions(array $board): array
+{
+    $to = [];
+    foreach (Util::$OFFSETS as $offset) {
+        foreach (array_keys($board) as $pos) {
+            list($p, $q) = explode(',', $pos);
+            $newPos = ($offset[0] + $p) . ',' . ($offset[1] + $q);
+            $to[] = $newPos;
+        }
     }
+
+    return array_unique($to) ?: ['0,0'];
 }
-$to = array_unique($to);
-if (!count($to)) {
-    $to[] = '0,0';
-}
+
 ?>
 <!DOCTYPE html>
-<lang html>
 
-    <head>
-        <title>Hive</title>
-        <style>
-            div.board {
-                width: 60%;
-                height: 100%;
-                min-height: 500px;
-                float: left;
-                overflow: scroll;
-                position: relative;
+<head>
+    <title>Hive</title>
+    <style>
+        div.board {
+            width: 60%;
+            height: 100%;
+            min-height: 500px;
+            float: left;
+            overflow: scroll;
+            position: relative;
+        }
+
+        div.board div.tile {
+            position: absolute;
+        }
+
+        div.tile {
+            display: inline-block;
+            width: 4em;
+            height: 4em;
+            border: 1px solid black;
+            box-sizing: border-box;
+            font-size: 50%;
+            padding: 2px;
+        }
+
+        div.tile span {
+            display: block;
+            width: 100%;
+            text-align: center;
+            font-size: 200%;
+        }
+
+        div.player0 {
+            color: black;
+            background: white;
+        }
+
+        div.player1 {
+            color: white;
+            background: black
+        }
+
+        div.stacked {
+            border-width: 3px;
+            border-color: red;
+            padding: 0;
+        }
+    </style>
+</head>
+
+<body>
+    <div class="board">
+        <?php
+        $min_p = 1000;
+        $min_q = 1000;
+        foreach ($board as $pos => $tile) {
+            $pq = explode(',', $pos);
+            if ($pq[0] < $min_p) {
+                $min_p = $pq[0];
             }
-
-            div.board div.tile {
-                position: absolute;
+            if ($pq[1] < $min_q) {
+                $min_q = $pq[1];
             }
-
-            div.tile {
-                display: inline-block;
-                width: 4em;
-                height: 4em;
-                border: 1px solid black;
-                box-sizing: border-box;
-                font-size: 50%;
-                padding: 2px;
+        }
+        foreach (array_filter($board) as $pos => $tile) {
+            $pq = explode(',', $pos);
+            $pq[0];
+            $pq[1];
+            $h = count($tile);
+            echo '<div class="tile player';
+            echo $tile[$h - 1][0];
+            if ($h > 1) {
+                echo ' stacked';
             }
-
-            div.tile span {
-                display: block;
-                width: 100%;
-                text-align: center;
-                font-size: 200%;
+            echo '" style="left: ';
+            echo ($pq[0] - $min_p) * 4 + ($pq[1] - $min_q) * 2;
+            echo 'em; top: ';
+            echo ($pq[1] - $min_q) * 4;
+            echo "em;\">($pq[0],$pq[1])<span>";
+            echo $tile[$h - 1][1];
+            echo '</span></div>';
+        }
+        ?>
+    </div>
+    <div class="hand">
+        White:
+        <?php
+        foreach ($hand[0] as $tile => $ct) {
+            for ($i = 0; $i < $ct; $i++) {
+                echo '<div class="tile player0"><span>' . $tile . "</span></div> ";
             }
-
-            div.player0 {
-                color: black;
-                background: white;
+        }
+        ?>
+    </div>
+    <div class="hand">
+        Black:
+        <?php
+        foreach ($hand[1] as $tile => $ct) {
+            for ($i = 0; $i < $ct; $i++) {
+                echo '<div class="tile player1"><span>' . $tile . "</span></div> ";
             }
-
-            div.player1 {
-                color: white;
-                background: black
-            }
-
-            div.stacked {
-                border-width: 3px;
-                border-color: red;
-                padding: 0;
-            }
-        </style>
-    </head>
-
-    <body>
-        <div class="board">
+        }
+        ?>
+    </div>
+    <div class="turn">
+        Turn:
+        <?php if ($player == 0) {
+            echo "White";
+        } else {
+            echo "Black";
+        }
+        ?>
+    </div>
+    <form method="post" action="play.php">
+        <select name="piece">
             <?php
-            $min_p = 1000;
-            $min_q = 1000;
-            foreach ($board as $pos => $tile) {
-                $pq = explode(',', $pos);
-                if ($pq[0] < $min_p) {
-                    $min_p = $pq[0];
-                }
-                if ($pq[1] < $min_q) {
-                    $min_q = $pq[1];
-                }
-            }
-            foreach (array_filter($board) as $pos => $tile) {
-                $pq = explode(',', $pos);
-                $pq[0];
-                $pq[1];
-                $h = count($tile);
-                echo '<div class="tile player';
-                echo $tile[$h - 1][0];
-                if ($h > 1) {
-                    echo ' stacked';
-                }
-                echo '" style="left: ';
-                echo ($pq[0] - $min_p) * 4 + ($pq[1] - $min_q) * 2;
-                echo 'em; top: ';
-                echo ($pq[1] - $min_q) * 4;
-                echo "em;\">($pq[0],$pq[1])<span>";
-                echo $tile[$h - 1][1];
-                echo '</span></div>';
+            foreach ($hand[$player] as $tile => $ct) {
+                echo "<option value=\"$tile\">$tile</option>";
             }
             ?>
-        </div>
-        <div class="hand">
-            White:
+        </select>
+        <select name="to">
             <?php
-            foreach ($hand[0] as $tile => $ct) {
-                for ($i = 0; $i < $ct; $i++) {
-                    echo '<div class="tile player0"><span>' . $tile . "</span></div> ";
-                }
+            foreach ($to as $pos) {
+                echo "<option value=\"$pos\">$pos</option>";
             }
             ?>
-        </div>
-        <div class="hand">
-            Black:
+        </select>
+        <input type="submit" value="Play">
+    </form>
+    <form method="post" action="move.php">
+        <select name="from">
             <?php
-            foreach ($hand[1] as $tile => $ct) {
-                for ($i = 0; $i < $ct; $i++) {
-                    echo '<div class="tile player1"><span>' . $tile . "</span></div> ";
-                }
+            foreach (array_keys($board) as $pos) {
+                echo "<option value=\"$pos\">$pos</option>";
             }
             ?>
-        </div>
-        <div class="turn">
-            Turn:
-            <?php if ($player == 0) {
-                echo "White";
-            } else {
-                echo "Black";
-            }
-            ?>
-        </div>
-        <form method="post" action="play.php">
-            <select name="piece">
-                <?php
-                foreach ($hand[$player] as $tile => $ct) {
-                    echo "<option value=\"$tile\">$tile</option>";
-                }
-                ?>
-            </select>
-            <select name="to">
-                <?php
-                foreach ($to as $pos) {
-                    echo "<option value=\"$pos\">$pos</option>";
-                }
-                ?>
-            </select>
-            <input type="submit" value="Play">
-        </form>
-        <form method="post" action="move.php">
-            <select name="from">
-                <?php
-                foreach (array_keys($board) as $pos) {
-                    echo "<option value=\"$pos\">$pos</option>";
-                }
-                ?>
-            </select>
-            <select name="to">
-                <?php
-                foreach ($to as $pos) {
-                    echo "<option value=\"$pos\">$pos</option>";
-                }
-                ?>
-            </select>
-            <input type="submit" value="Move">
-        </form>
-        <form method="post" action="pass.php">
-            <input type="submit" value="Pass">
-        </form>
-        <form method="post" action="restart.php">
-            <input type="submit" value="Restart">
-        </form>
-        <strong>
-            <?php if (isset($_SESSION['error'])) {
-                echo $_SESSION['error'];
-            }
-            unset($_SESSION['error']); ?>
-        </strong>
-        <ol>
+        </select>
+        <select name="to">
             <?php
-            $db = DatabaseConnection::getInstance();
-            $stmt = $db->prepare('SELECT * FROM moves WHERE game_id = ' . $_SESSION['game_id']);
-            $stmt->execute();
-            $result = $stmt->get_result();
-            while ($row = $result->fetch_array()) {
-                echo '<li>' . $row[2] . ' ' . $row[3] . ' ' . $row[4] . '</li>';
+            foreach ($to as $pos) {
+                echo "<option value=\"$pos\">$pos</option>";
             }
             ?>
-        </ol>
-        <form method="post" action="undo.php">
-            <input type="submit" value="Undo">
-        </form>
-    </body>
+        </select>
+        <input type="submit" value="Move">
+    </form>
+    <form method="post" action="pass.php">
+        <input type="submit" value="Pass">
+    </form>
+    <form method="post" action="restart.php">
+        <input type="submit" value="Restart">
+    </form>
+    <strong>
+        <?php if (isset($_SESSION['error'])) {
+            echo $_SESSION['error'];
+        }
+        unset($_SESSION['error']); ?>
+    </strong>
+    <ol>
+        <?php
+        $db = DatabaseConnection::getInstance();
+        $stmt = $db->prepare('SELECT * FROM moves WHERE game_id = ' . $_SESSION['game_id']);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        while ($row = $result->fetch_array()) {
+            echo '<li>' . $row[2] . ' ' . $row[3] . ' ' . $row[4] . '</li>';
+        }
+        ?>
+    </ol>
+    <form method="post" action="undo.php">
+        <input type="submit" value="Undo">
+    </form>
+</body>
 
-    </html>
+</html>
